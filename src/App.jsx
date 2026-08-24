34b61128f4bf66b703116a780def4cbe2fef553e
Poovarasanm8870-lgtm <poovarasanm8870@gmail.com>
2026-08-24 13:40:45 +0530

    Fix plain page styling & routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { EventProvider, useEvents } from './context/EventContext';
 import { CustomCursor } from './components/common/CustomCursor';
 import { Navbar } from './components/common/Navbar';
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 import App from './App.jsx'
 import './index.css'
 
@@ -8,4 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
-
